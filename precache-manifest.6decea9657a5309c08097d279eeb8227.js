f9db445642a9db236a8c0a3fff352db46d0eb6aa
Shwetha DS <dss.music24@gmail.com>
2020-10-03 13:34:52 +0530

    Updates

---
+++ precache-manifest.6decea9657a5309c08097d279eeb8227.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966b120e7ba0e646af413cb59717fafc",
+    "url": "/robotots/index.html"
+  },
+  {
+    "revision": "c39a5e9f1485ad59f707",
+    "url": "/robotots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4ddc7887e0eba42f267d",
+    "url": "/robotots/static/css/main.7f2c6f3e.chunk.css"
+  },
+  {
+    "revision": "c39a5e9f1485ad59f707",
+    "url": "/robotots/static/js/2.eafa6f66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotots/static/js/2.eafa6f66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ddc7887e0eba42f267d",
+    "url": "/robotots/static/js/main.1eca97ad.chunk.js"
+  },
+  {
+    "revision": "629d937beb1b60c7c3cf",
+    "url": "/robotots/static/js/runtime-main.a0b947c5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
